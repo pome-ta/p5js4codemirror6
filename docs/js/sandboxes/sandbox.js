919eb77f78e57cd9200c818f321e2ebc888eb2ce
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-07-13 19:00:13 +0900

    not work

--- docs/js/sandboxes/sandbox.js
+++ docs/js/sandboxes/sandbox.js
@@ -1,10 +1,28 @@
+import p5Origin from 'p5/';
 import DomFactory from '../utils/domFactory.js';
+
+window._p5Instance = null;
+
+function importRefresh() {
+
+  class p5 extends p5Origin {
+    constructor(sketch, node) {
+      super(sketch, node);
+      window._p5Instance = this;
+    }
+  }
+
+  window.p5 = p5;
+
+}
 
 function runSketch(code) {
   if (window._p5Instance) {
     window._p5Instance.remove();
     window._p5Instance = null;
   }
+  importRefresh();
+
 
   const script = DomFactory.create('script', {
     setAttrs: {
@@ -20,7 +38,7 @@ function runSketch(code) {
 
   if (window._p5Instance === null) {
     try {
-      //window._p5Instance = new p5();
+      window._p5Instance = new p5();
     } catch (e) {
       console.log('Error: ' + e.message);
     }
@@ -34,17 +52,20 @@ window.addEventListener('message', (e) => {
   runSketch(sourceCode);
 });
 
-window._p5Instance = null;
+importRefresh();
+// await import('p5/lib/addons/p5.sound');
 
-window.__p5 = window.p5;
-delete window.p5;
-
-class p5 extends window.__p5 {
-  constructor(sketch, node) {
-    super(sketch, node);
-    window._p5Instance = this;
-  }
-}
-
-window.p5 = p5;
-delete window.__p5;
+// window._p5Instance = null;
+//
+// window.__p5 = window.p5;
+// delete window.p5;
+//
+// class p5 extends window.__p5 {
+//   constructor(sketch, node) {
+//     super(sketch, node);
+//     window._p5Instance = this;
+//   }
+// }
+//
+// window.p5 = p5;
+// delete window.__p5;
