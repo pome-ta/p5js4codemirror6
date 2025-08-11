1e4957ef98d14686270cbffc711c5e44bd5281ee
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-11 13:50:42 +0900

    Update devSketch.js

--- docs/js/sketchBooks/devSketch.js
+++ docs/js/sketchBooks/devSketch.js
@@ -1,111 +1,50 @@
-const spectrumAnalyzerPath = '../../sketchBooks/modules/spectrumAnalyzer.js';
-const interactionTraceKitPath =
-  '../../sketchBooks/modules/interactionTraceKit.js';
+// [002-Amplitude-VisualizingLoudness by thomasjohnmartinez -p5.js Web Editor](https://editor.p5js.org/thomasjohnmartinez/sketches/Wlcnc6WCD)
+const soundPath = 'https://tonejs.github.io/audio/berklee/gong_1.mp3';
 
 const sketch = (p) => {
+
   let w = p.windowWidth;
   let h = p.windowHeight;
-  let bgColor;
 
-  let spectrumAnalyzer;
-  let pointerTracker;
-  let tapIndicator;
-
-  const BPM = 90;
-
-  let fft;
-
-  let osc;
-  let env;
-  let phrase;
-  let part;
-
-  // todo: `0` に近い、最小値として
-  const zero = 1e-3 + 1e-4;
+  let sound;
+  let amp;
 
   p.preload = () => {
-    p.loadModule(spectrumAnalyzerPath, (m) => {
-      const SpectrumAnalyzer = m.default;
-      spectrumAnalyzer = new SpectrumAnalyzer(p);
-    });
-    p.loadModule(interactionTraceKitPath, (m) => {
-      const {PointerTracker, TapIndicator} = m;
-      pointerTracker = new PointerTracker(p);
-      tapIndicator = new TapIndicator(p);
-    });
+    sound = p.loadSound(soundPath);
   };
+
 
   p.setup = () => {
     // put setup code here
     soundReset();
 
-    p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {
-      passive: false,
-    });
+    const cnv = p.createCanvas(w, h);
+    cnv.mousePressed(playSound);
 
-    p.createCanvas(w, h);
-    p.colorMode(p.HSL, 1, 1, 1);
+    p.textAlign(p.CENTER);
+    p.fill(255);
 
-    bgColor = [0, 0, 0.25];
-    p.background(...bgColor);
+    amp = new p5.Amplitude();
+    sound.connect(amp);
 
-    fft = new p5.FFT();
-    spectrumAnalyzer.setup(fft);
-    tapIndicator.setup();
+    p.describe('The color of the background changes based on the amplitude of the sound.');
 
-    // --- sound
-    p.setBPM(BPM);
 
-    const types = ['sine', 'triangle', 'sawtooth', 'square'];
-    osc = new p5.Oscillator(types[0]);
-    osc.start();
-    osc.amp(0);
-
-    env = new p5.Envelope();
-    env.setADSR(zero, 0.1, 1, zero + zero);
-    env.setExp(true);
-
-    phrase = new p5.Phrase(
-      'metronom',
-      (time, playbackRate) => {
-        if (!playbackRate) {
-          return;
-        }
-
-        osc.freq(playbackRate);
-        env.play(osc);
-      },
-      [
-        880, 0, 0, 0,
-        440, 0, 0, 0,
-        440, 0, 0, 0,
-        440, 0, 0, 0,
-      ]
-    );
-
-    part = new p5.Part();
-    part.addPhrase(phrase);
-    part.loop();
-
-    window._cacheSounds = [osc, env, part];
+    window._cacheSounds = [sound, amp,];
   };
 
   p.draw = () => {
     // put drawing code here
-    p.background(...bgColor);
-
-    const spectrum = fft.analyze();
-    spectrumAnalyzer.drawSpectrum(spectrum);
+    const level = p.map(amp.getLevel(), 0, 0.2, 0, 225);
+    p.background(level, 0, 0);
+    p.text('click to play', w / 2, 120);
   };
 
-  p.touchStarted = (e) => {
-  };
 
-  p.touchMoved = (e) => {
-  };
+  function playSound() {
+    sound.play();
+  }
 
-  p.touchEnded = (e) => {
-  };
 
   p.windowResized = (e) => {
     w = p.windowWidth;
