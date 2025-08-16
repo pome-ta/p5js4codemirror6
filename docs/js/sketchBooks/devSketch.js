57e420db4bb60712c861a2b48ea380062a8f216c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-16 15:51:37 +0900

    Update devSketch.js

--- docs/js/sketchBooks/devSketch.js
+++ docs/js/sketchBooks/devSketch.js
@@ -7,82 +7,86 @@ const interactionTraceKitPath =
 const sketch = (p) => {
   let w = p.windowWidth;
   let h = p.windowHeight;
-  
+
   let pointerTracker;
   let pointX, pointY;
 
   let osc;
   let delay;
   let env;
-  
+
   p.preload = () => {
     p.loadModule(interactionTraceKitPath, (m) => {
-      const { PointerTracker } = m;
+      const {PointerTracker} = m;
       pointerTracker = new PointerTracker(p);
     });
   };
-  
+
 
   p.setup = () => {
     // put setup code here
     soundReStart();
-    
+
     p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {
       passive: false,
     });
 
     const cnv = p.createCanvas(w, h);
     p.background(220);
-    
+
     p.textAlign(p.CENTER);
     p.textSize(13);
     p.text('click and drag mouse', w / 2, h / 2);
-    
+
 
     osc = new p5.Oscillator('sawtooth');
-    osc.amp(0.75);
-    
-    env = new p5.Envelope(0.01);
-    delay = new p5.Delay(0.12, 0.7);
-    
-    osc.amp(env);
-    osc.disconnect();
-    osc.connect(delay);
-    
-    /*
-    cnv.mousePressed(oscStart);
-    cnv.mouseReleased(oscStop);
-    cnv.mouseOut(oscStop);
-    */
-    
-    
+    osc.amp(0.5);
+
+    env = new p5.Envelope();
+    env.setADSR(0.01, 0, 1.0);
+    // env.setRange();
+    delay = new p5.Delay();
+    delay.process(osc, 0.12, 0.7);
+
+    // osc.disconnect();
+    // osc.connect(env);
+
+    // osc.amp(env);
+    // osc.disconnect();
+    // osc.connect(delay);
+
+
+    // cnv.mousePressed(oscStart);
+    // cnv.mouseReleased(oscStop);
+    // cnv.mouseOut(oscStop);
+
+
     p.describe('Click and release or hold, to play a square wave with delay effect.');
-    
+
   };
 
   p.draw = () => {
     // put drawing code here
     osc.freq(p.map(p.mouseY, h, 0, 440, 880));
-    
+
     const dtime = p.map(p.mouseX, 0, w, 0.1, 0.5);
     delay.delayTime(dtime);
   };
 
   function oscStart() {
     p.background(0, 255, 255);
-    p.text('release to hear delay', w/2, h/2);
+    p.text('release to hear delay', w / 2, h / 2);
     osc.start();
-    env.triggerAttack();
+    env.triggerAttack(osc);
   }
-  
+
   function oscStop() {
     p.background(220);
-    p.text('click and drag mouse', w/2, h/2);
-    env.triggerRelease();
+    p.text('click and drag mouse', w / 2, h / 2);
+    env.triggerRelease(osc);
   }
-  
-  
-  
+
+
   p.touchStarted = (e) => {
     oscStart();
   };
@@ -93,10 +97,7 @@ const sketch = (p) => {
   p.touchEnded = (e) => {
     oscStop();
   };
-  
 
-
-  
 
   p.windowResized = (e) => {
     w = p.windowWidth;
