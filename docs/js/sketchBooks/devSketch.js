3ef58a09ee86aed2f45e840560f60c00eb3f7939
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-19 13:41:12 +0900

    up

--- docs/js/sketchBooks/devSketch.js
+++ docs/js/sketchBooks/devSketch.js
@@ -1,101 +1,121 @@
-// [p5.js Web Editor | 006-DelayTime-Envelope](https://editor.p5js.org/thomasjohnmartinez/sketches/Dk95S298f)
+// `soundReset` の改善
 
-const interactionTraceKitPath =
-  '../../sketchBooks/modules/interactionTraceKit.js';
+const spectrumAnalyzerPath = '../../sketchBooks/modules/spectrumAnalyzer.js';
+const interactionTraceKitPath = '../../sketchBooks/modules/interactionTraceKit.js';
 
 
 const sketch = (p) => {
   let w = p.windowWidth;
   let h = p.windowHeight;
 
+  let SpectrumAnalyzer;
   let pointerTracker;
-  let pointX, pointY;
+  let tapIndicator;
+
+  const BPM = 90;
+
+  let fft;
 
   let osc;
-  let delay;
   let env;
+  let phrase;
+  let part;
+
+  let osca;
+  let lfo;
+  let oscb;
+
+  // todo: `0` に近い、最小値として
+  const zero = 1e-3 + 1e-4;
 
   p.preload = () => {
+    p.loadModule(spectrumAnalyzerPath, (m) => {
+      const SpectrumAnalyzer = m.default;
+      spectrumAnalyzer = new SpectrumAnalyzer(p);
+    });
     p.loadModule(interactionTraceKitPath, (m) => {
-      const {PointerTracker} = m;
+      const {PointerTracker, TapIndicator} = m;
       pointerTracker = new PointerTracker(p);
+      tapIndicator = new TapIndicator(p);
     });
   };
 
-
   p.setup = () => {
     // put setup code here
-    soundReStart();
+    soundReset();
 
     p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {
       passive: false,
     });
 
-    const cnv = p.createCanvas(w, h);
-    p.background(220);
+    p.createCanvas(w, h);
+    p.colorMode(p.HSL, 1, 1, 1);
 
-    p.textAlign(p.CENTER);
-    p.textSize(13);
-    p.text('click and drag mouse', w / 2, h / 2);
+    bgColor = [0, 0, 0.25];
+    p.background(...bgColor);
 
+    fft = new p5.FFT();
 
-    osc = new p5.Oscillator('sawtooth');
-    osc.amp(0.5);
+    spectrumAnalyzer.setup(fft);
+    //tapIndicator.setup();
+
+    // --- sound
+    p.setBPM(BPM);
+
+    const types = ['sine', 'triangle', 'sawtooth', 'square'];
+    osc = new p5.Oscillator(types[0]);
+    osc.start();
+    osc.amp(0);
 
     env = new p5.Envelope();
-    env.setADSR(0.01, 0, 1.0);
-    // env.setRange();
-    delay = new p5.Delay();
-    delay.process(osc, 0.12, 0.3);
+    env.setADSR(zero, 0.1, 1, zero + zero);
+    env.setExp(true);
 
-    // osc.disconnect();
-    // osc.connect(env);
+    phrase = new p5.Phrase(
+      'metronom',
+      (time, playbackRate) => {
+        if (!playbackRate) {
+          return;
+        }
 
-    // osc.amp(env);
-    // osc.disconnect();
-    // osc.connect(delay);
+        osc.freq(playbackRate, 0.5);
+        env.play(osc);
+      },
+      [880, 0, 0, 0, 440, 0, 0, 0, 440, 0, 0, 0, 440, 0, 0, 0]
+    );
+
+    part = new p5.Part();
+    part.addPhrase(phrase);
+    part.loop();
 
 
-    // cnv.mousePressed(oscStart);
-    // cnv.mouseReleased(oscStop);
-    // cnv.mouseOut(oscStop);
+    osca = new p5.Oscillator(types[0], 440 + (p.random() * 440));
+    osca.aname = 'a';
+
+    //osc.amp(0.4);
+    //osca.start();
+
+    lfo = new p5.Oscillator(0.3, 'sine'); // 速さ
+    lfo.amp(500); // 幅
+    lfo.start();
+    osca.start();
+    lfo.disconnect();
+    lfo.connect(osca.freqNode);
 
 
-    p.describe('Click and release or hold, to play a square wave with delay effect.');
-
+    oscb = new p5.Oscillator(types[1], 880 + (p.random() * 440));
+    oscb.aname = 'b';
+    oscb.amp(0.4);
+    oscb.start();
   };
 
   p.draw = () => {
     // put drawing code here
-    osc.freq(p.map(p.mouseY, h, 0, 440, 880));
+    p.background(...bgColor);
 
-    const dtime = p.map(p.mouseX, 0, w, 0.1, 0.5);
-    delay.delayTime(dtime);
-  };
+    const spectrum = fft.analyze();
+    spectrumAnalyzer.drawSpectrum(spectrum);
 
-  function oscStart() {
-    p.background(0, 255, 255);
-    p.text('release to hear delay', w / 2, h / 2);
-    osc.start();
-    env.triggerAttack(osc);
-  }
-
-  function oscStop() {
-    p.background(220);
-    p.text('click and drag mouse', w / 2, h / 2);
-    env.triggerRelease(osc);
-  }
-
-
-  p.touchStarted = (e) => {
-    oscStart();
-  };
-
-  p.touchMoved = (e) => {
-  };
-
-  p.touchEnded = (e) => {
-    oscStop();
   };
 
 
@@ -105,14 +125,14 @@ const sketch = (p) => {
     p.resizeCanvas(w, h);
   };
 
-  function soundReStart() {
-    // wip: クリップノイズ対策
+  function soundReset() {
+    // wip: クリップノイズ対策
     p.disposeSound();
 
     const soundArray = p.soundOut.soundArray;
     for (let soundIdx = soundArray.length - 1; soundIdx >= 0; soundIdx--) {
       const sound = soundArray[soundIdx];
-      // todo: 過剰処理？
+      // todo: 過剰処理?
       sound?.stop && sound.stop();
       sound?.dispose && sound.dispose();
       sound?.disconnect && sound.disconnect();
@@ -131,7 +151,7 @@ const sketch = (p) => {
 
     p.soundOut.soundArray = [];
     p.soundOut.parts = [];
-    p.soundOut.extensions = []; // todo: 対応必要？
+    p.soundOut.extensions = [];  // todo: 対応必要?
 
     p.userStartAudio();
   }
