540eed241d15ffe4b8427183bc4aa6f3bef27e43
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-20 16:30:31 +0900

    up

--- docs/js/sketchBooks/devSketch.js
+++ docs/js/sketchBooks/devSketch.js
@@ -1,86 +1,96 @@
-// p5.sound のv0.2 確認
+// [p5.js Web Editor | 006-DelayTime-Envelope](https://editor.p5js.org/thomasjohnmartinez/sketches/Dk95S298f)
 
-//const spectrumAnalyzerPath = '../../sketchBooks/modules/spectrumAnalyzer.js';
-const interactionTraceKitPath = '../../sketchBooks/modules/interactionTraceKit.js';
-
+const interactionTraceKitPath =
+  '../../sketchBooks/modules/interactionTraceKit.js';
 
 const sketch = (p) => {
   let w = p.windowWidth;
   let h = p.windowHeight;
 
-  
-  //let SpectrumAnalyzer;
   let pointerTracker;
-  let tapIndicator;
+  let pointX, pointY;
 
   let osc;
-  let osca,lfo,oscb;
-  
-  
+  let delay;
+  let env;
+
   p.preload = () => {
-    /*
-    p.loadModule(spectrumAnalyzerPath, (m) => {
-      const SpectrumAnalyzer = m.default;
-      spectrumAnalyzer = new SpectrumAnalyzer(p);
-    });
-    */
     p.loadModule(interactionTraceKitPath, (m) => {
-      const {PointerTracker, TapIndicator} = m;
+      const { PointerTracker } = m;
       pointerTracker = new PointerTracker(p);
-      tapIndicator = new TapIndicator(p);
     });
   };
-  
 
   p.setup = () => {
     // put setup code here
-    soundReset();
+    soundReStart();
 
     p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {
       passive: false,
     });
 
-    p.createCanvas(w, h);
-    p.colorMode(p.HSL, 1, 1, 1);
+    const cnv = p.createCanvas(w, h);
+    p.background(220);
 
-    bgColor = [0, 0, 0.25];
-    p.background(...bgColor);
+    p.textAlign(p.CENTER);
+    p.textSize(13);
+    p.text('click and drag mouse', w / 2, h / 2);
 
-    
-    // --- sound
-    const types = ['sine', 'triangle', 'sawtooth', 'square'];
-    /*
-    osca = new p5.Oscillator(types[0], 440 + (p.random() * 440));
-    osca.aname = 'a';
+    osc = new p5.Oscillator('sawtooth');
+    osc.amp(0.5);
 
-    //osc.amp(0.4);
-    //osca.start();
+    env = new p5.Envelope();
+    env.setADSR(0.01, 0, 1.0);
+    // env.setRange();
+    delay = new p5.Delay();
+    delay.process(osc, 0.12, 0.7);
 
-    lfo = new p5.Oscillator(0.3, 'sine'); // 速さ
-    lfo.amp(500); // 幅
-    lfo.start();
-    osca.start();
-    lfo.disconnect();
-    lfo.connect(osca.osc);
-    */
+    // osc.disconnect();
+    // osc.connect(env);
 
+    // osc.amp(env);
+    // osc.disconnect();
+    // osc.connect(delay);
 
-    oscb = new p5.Oscillator(types[1], 880 + (p.random() * 440));
-    oscb.aname = 'b';
-    oscb.amp(0.4);
-    oscb.start();
-    console.log(p)
+    // cnv.mousePressed(oscStart);
+    // cnv.mouseReleased(oscStop);
+    // cnv.mouseOut(oscStop);
+
+    p.describe(
+      'Click and release or hold, to play a square wave with delay effect.'
+    );
   };
 
   p.draw = () => {
     // put drawing code here
-    p.background(...bgColor);
+    osc.freq(p.map(p.mouseY, h, 0, 440, 880));
 
-    //const spectrum = fft.analyze();
-    //spectrumAnalyzer.drawSpectrum(spectrum);
-
+    const dtime = p.map(p.mouseX, 0, w, 0.1, 0.5);
+    delay.delayTime(dtime);
   };
 
+  function oscStart() {
+    p.background(0, 255, 255);
+    p.text('release to hear delay', w / 2, h / 2);
+    osc.start();
+    env.triggerAttack(osc);
+  }
+
+  function oscStop() {
+    p.background(220);
+    p.text('click and drag mouse', w / 2, h / 2);
+    env.triggerRelease(osc);
+  }
+
+  p.touchStarted = (e) => {
+    oscStart();
+  };
+
+  p.touchMoved = (e) => {};
+
+  p.touchEnded = (e) => {
+    oscStop();
+  };
 
   p.windowResized = (e) => {
     w = p.windowWidth;
@@ -88,9 +98,8 @@ const sketch = (p) => {
     p.resizeCanvas(w, h);
   };
 
-  function soundReset() {
+  function soundReStart() {
     // wip: クリップノイズ対策
-    /*
     p.disposeSound();
 
     const soundArray = p.soundOut.soundArray;
@@ -115,10 +124,8 @@ const sketch = (p) => {
 
     p.soundOut.soundArray = [];
     p.soundOut.parts = [];
-    p.soundOut.extensions = [];  // todo: 対応必要?
-    */
-    
-    p.setAudioContext()
+    p.soundOut.extensions = []; // todo: 対応必要?
+
     p.userStartAudio();
   }
 };
