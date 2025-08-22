bfb4d59d73359086b2e0880c3157b52ad1ed8606
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-22 11:35:33 +0900

    up

--- docs/js/sketchBooks/devSketch.js
+++ docs/js/sketchBooks/devSketch.js
@@ -1,4 +1,12 @@
-// [p5.js-sound/examples/array_of_notes/sketch.js at main · processing/p5.js-sound · GitHub](https://github.com/processing/p5.js-sound/blob/main/examples/array_of_notes/sketch.js)
+// [p5.js-sound/examples/virtual_piano/sketch.js at main · processing/p5.js-sound · GitHub](https://github.com/processing/p5.js-sound/blob/main/examples/virtual_piano/sketch.js)
+/**
+ * Virtual Piano: Example for event-driven sound playback.
+ * バーチャルピアノ: イベント駆動型のサウンド再生の例。
+ * 
+ * This example uses the p5.PolySynth to produce instantaneously,
+ * driven by user events.
+ * この例では、p5.PolySynth を使用して、ユーザー イベントによって瞬時に生成します。
+ */
 
 const interactionTraceKitPath =
   '../../sketchBooks/modules/interactionTraceKit.js';
@@ -10,28 +18,11 @@ const sketch = (p) => {
   let pointerTracker;
   let tapIndicator;
 
-  let synth;
-  let songStarted = false;
-  
-  const song = [
-    // Note pitch, velocity (between 0-1), start time (s), note duration (s)
-    { pitch: 'E4', velocity: 1, time: 0, duration: 1 },
-    { pitch: 'D4', velocity: 1, time: 1, duration: 1 },
-    { pitch: 'C4', velocity: 1, time: 2, duration: 1 },
-    { pitch: 'D4', velocity: 1, time: 3, duration: 1 },
-    { pitch: 'E4', velocity: 1, time: 4, duration: 1 },
-    { pitch: 'E4', velocity: 1, time: 5, duration: 1 },
-    { pitch: 'E4', velocity: 1, time: 6, duration: 1 },
-    // Rest indicated by offset in start time
-    { pitch: 'D4', velocity: 1, time: 8, duration: 1 },
-    { pitch: 'D4', velocity: 1, time: 9, duration: 1 },
-    { pitch: 'E4', velocity: 1, time: 10, duration: 1 },
-    { pitch: 'D4', velocity: 1, time: 11, duration: 1 },
-    // Chord indicated by simultaneous note start times
-    { pitch: 'C4', velocity: 1, time: 12, duration: 2 },
-    { pitch: 'E4', velocity: 1, time: 12, duration: 2 },
-    { pitch: 'G4', velocity: 1, time: 12, duration: 2 },
-  ];
+  let polySynth;
+  const velocity = 0.7; // From 0-1
+  const baseNote = 72;
+  const keyOrder = "ASDFGHJKL";
+  const keyStates = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   
 
   p.preload = () => {
@@ -53,34 +44,56 @@ const sketch = (p) => {
 
     p.createCanvas(w, h);
     tapIndicator.setup();
+    
 
     p.textAlign(p.CENTER, p.CENTER);
+    p.strokeWeight(3);
     
     // --- sound
+    // Create synth voice
     synth = new p5.PolySynth();
+    
+    
     
   };
 
   p.draw = () => {
     // put drawing code here
-    p.background(180);
-    songStarted ? p.text('song started', w / 2, h / 2) : p.text('click to play song', w / 2, h / 2);
+    const keyWidth = w / keyStates.length;
+    
+    for (let i = 0; i < keyStates.length; i++) {
+      const keyColor = keyStates[i] === 1 ? p.color(255, 220, 120) : p.color(150, 230, 245);
+      p.fill(keyColor);
+      p.stroke(255);
+      
+      p.rect(i * keyWidth, 0, keyWidth, h);
+      // Key label
+      p.fill(40);
+      p.noStroke();
+      p.text(keyOrder[i], i * keyWidth + keyWidth / 2, h / 2);
+    }
+    
   };
 
   p.touchStarted = (e) => {
-    if (!songStarted) { // Only play once
-      for (let i = 0; i < song.length; i++) {
-        const note = song[i];
-        synth.play(note.pitch, note.velocity, note.time, note.duration);
-      }
-      songStarted = true;
-    }
+    const keyWidth = w / keyStates.length;
+    const keyIndex = p.floor(p.mouseX / keyWidth);
+    // Update key state
+    keyStates[keyIndex] = 1;
+    // Play synth
+    const midiNoteNumber = baseNote + keyIndex; // 0-127; 60 is Middle C (C4)
+    const freq = p.midiToFreq(midiNoteNumber);
+    synth.noteAttack(freq, velocity, 0);
   };
 
   p.touchMoved = (e) => {
   };
 
   p.touchEnded = (e) => {
+    for (let i = 0; i < keyStates.length; i++) {
+      keyStates[i] = 0;
+    }
+    synth.noteRelease();
   };
 
   p.windowResized = (e) => {
