2b554e61d10f98be4ae8ac77b2a2081a7a0ac67a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-25 11:44:49 +0900

    load

--- docs/js/sketchBooks/devSketch.js
+++ docs/js/sketchBooks/devSketch.js
@@ -1,163 +1,31 @@
-// [p5.js-sound/examples/virtual_piano/sketch.js at main · processing/p5.js-sound · GitHub](https://github.com/processing/p5.js-sound/blob/main/examples/virtual_piano/sketch.js)
-/**
- * Virtual Piano: Example for event-driven sound playback.
- * バーチャルピアノ: イベント駆動型のサウンド再生の例。
- * 
- * This example uses the p5.PolySynth to produce instantaneously,
- * driven by user events.
- * この例では、p5.PolySynth を使用して、ユーザー イベントによって瞬時に生成します。
- */
+let song;
+const url = 'https://github.com/processing/p5.js-sound/blob/main/examples/files/Damscray_DancingTiger.ogg';
 
-const interactionTraceKitPath =
-  '../../sketchBooks/modules/interactionTraceKit.js';
+function preload() {
+  // 外部URLから音源をロード
+  const githubusercontent = (githubUrl) => githubUrl.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('/blob/', '/');
+  song = loadSound(githubusercontent(url));
+}
 
-const sketch = (p) => {
-  let w = p.windowWidth;
-  let h = p.windowHeight;
+function setup() {
+  createCanvas(400, 200);
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  text('クリックで再生', width/2, height/2);
+}
 
-  let pointerTracker;
-  let tapIndicator;
-
-  let polySynth;
-  const velocity = 0.7; // From 0-1
-  const baseNote = 72;
-  const keyOrder = "ASDFGHJKL";
-  const keyStates = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  
-
-  p.preload = () => {
-    p.loadModule(interactionTraceKitPath, (m) => {
-      const {PointerTracker, TapIndicator} = m;
-      pointerTracker = new PointerTracker(p);
-      tapIndicator = new TapIndicator(p);
-    });
-  };
-
-  p.setup = () => {
-    // put setup code here
-    soundReStart();
-
-    p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {
-      passive: false,
-    });
-    
-
-    p.createCanvas(w, h);
-    tapIndicator.setup();
-    
-
-    p.textAlign(p.CENTER, p.CENTER);
-    p.strokeWeight(3);
-    
-    // --- sound
-    // Create synth voice
-    synth = new p5.PolySynth();
-    
-  };
-
-  p.draw = () => {
-    // put drawing code here
-    const keyWidth = w / keyStates.length;
-    
-    for (let i = 0; i < keyStates.length; i++) {
-      const keyColor = keyStates[i] === 1 ? p.color(255, 220, 120) : p.color(150, 230, 245);
-      p.fill(keyColor);
-      p.stroke(255);
-      
-      p.rect(i * keyWidth, 0, keyWidth, h);
-      // Key label
-      p.fill(40);
-      p.noStroke();
-      p.text(keyOrder[i], i * keyWidth + keyWidth / 2, h / 2);
-    }
-  };
-  
-
-  function keyPressed() {
-    const keyIndex = keyOrder.indexOf(p.key);
-    // Check if valid note key pressed
-    if (keyIndex >= 0) {
-      // Update key state
-      keyStates[keyIndex] = 1;
-      // Play synth
-      const midiNoteNumber = baseNote + keyIndex; // 0-127; 60 is Middle C (C4)
-      const freq = p.midiToFreq(midiNoteNumber);
-      synth.noteAttack(freq, velocity, 0);
-    }
+function mousePressed() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
   }
-  
-  function keyReleased() {
-    const keyIndex = keyOrder.indexOf(p.key);
-    // Check if valid note key pressed
-    if (keyIndex >= 0) {
-      // Update key state
-      keyStates[keyIndex] = 0;
-      // Stop synth
-      const midiNoteNumber = baseNote + keyIndex; // 0-127; 60 is Middle C (C4)
-      const freq = p.midiToFreq(midiNoteNumber);
-      synth.noteRelease(freq, 0);
-    }
-  }
-  
+}
 
-  p.touchStarted = (e) => {
-    const keyWidth = w / keyStates.length;
-    const keyIndex = p.floor(p.mouseX / keyWidth);
-    // Update key state
-    keyStates[keyIndex] = 1;
-    // Play synth
-    const midiNoteNumber = baseNote + keyIndex; // 0-127; 60 is Middle C (C4)
-    const freq = p.midiToFreq(midiNoteNumber);
-    synth.noteAttack(freq, velocity, 0);
-  };
+// https://raw.githubusercontent.com/processing/p5.js-sound/main/examples/files/Damscray_DancingTiger.ogg
 
-  p.touchMoved = (e) => {
-  };
+// https://github.com/processing/p5.js-sound/blob/main/examples/files/Damscray_DancingTiger.ogg
 
-  p.touchEnded = (e) => {
-    for (let i = 0; i < keyStates.length; i++) {
-      keyStates[i] = 0;
-    }
-    synth.noteRelease();
-  };
 
-  p.windowResized = (e) => {
-    w = p.windowWidth;
-    h = p.windowHeight;
-    p.resizeCanvas(w, h);
-  };
-
-  function soundReStart() {
-    // wip: クリップノイズ対策
-    p.disposeSound();
-
-    const soundArray = p.soundOut.soundArray;
-    for (let soundIdx = soundArray.length - 1; soundIdx >= 0; soundIdx--) {
-      const sound = soundArray[soundIdx];
-      // todo: 過剰処理?
-      sound?.stop && sound.stop();
-      sound?.dispose && sound.dispose();
-      sound?.disconnect && sound.disconnect();
-
-      soundArray.splice(soundIdx, 1);
-    }
-
-    const parts = p.soundOut.parts;
-    for (let partIdx = parts.length - 1; partIdx >= 0; partIdx--) {
-      const phrases = parts[partIdx].phrases;
-      for (let phraseIdx = phrases.length - 1; phraseIdx >= 0; phraseIdx--) {
-        phrases.splice(phraseIdx, 1);
-      }
-      parts.splice(partIdx, 1);
-    }
-
-    p.soundOut.soundArray = [];
-    p.soundOut.parts = [];
-    p.soundOut.extensions = []; // todo: 対応必要?
-
-    p.userStartAudio();
-  }
-};
-
-new p5(sketch);
-
+//const rawUrl = blobUrl.replace("https://github.com/", "https://raw.githubusercontent.com/").replace("/blob/", "/");
+//const rawUrl = blobUrl.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('/blob/', '/');
