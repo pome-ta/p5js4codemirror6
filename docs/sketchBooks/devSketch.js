af45d670129d05c6742e6bae297d9ba79dd6865e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-12 23:38:44 +0900

    FFT_freqRange.js

--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -22,7 +22,6 @@ const githubusercontent = (githubUrl) =>
 
 const sketch = (p) => {
   let w, h;
-  let cnvs;
 
   let soundFile;
   let fft;
@@ -42,31 +41,72 @@ const sketch = (p) => {
     h = p.windowHeight;
 
     
-    p.createCanvas(w, h/2);
+    p.createCanvas(w, h / 2);
     
-    
-    //p.fill(255, 40, 255);
+    p.fill(255, 40, 255);
     p.noStroke();
     p.textAlign(p.CENTER);
+    p.textSize(8);
+    
     fft = new p5.FFT();
 
     pTag = p.createP(description);
     const p2Tag = p.createP('Description: Using getEnergy(low, high) to measure amplitude within a range of frequencies.');
     
     
-
-
-    
-
-    
   };
 
   p.draw = () => {
     // put drawing code here
-    //p.background(128);
+    p.background(30,20,30);
+    updateDescription();
+    
+    fft.analyze();
+    for (let i = 0; i < 8; i++) {
+      p.noStroke();
+      p.fill((i*30) % 100 + 50, 195, (i*25 + 50) % 255);
+      // Each bar has a unique frequency range
+      const centerFreq = (p.pow(2,i)*125)/2;
+      const loFreq = (p.pow(2,i-1)*125)/2 + centerFreq/4;
+      const hiFreq = (centerFreq + centerFreq/2);
+      
+      const freqValue = fft.getEnergy(loFreq, hiFreq - 1);
+      // Rectangle height represents the average value of this frequency range
+      const _h = (-h/2) + p.map(freqValue, 0, 255, h/2, 0);
+      p.rect((i+1)*w/8 - w/8, h/2, w/8, _h);
+  
+      p.fill(255);
+      p.text( loFreq.toFixed(0) +' Hz - ' + hiFreq.toFixed(0)+' Hz', (i+1)*w/8 - w/8/2, 30);
+    }
 
     
   };
+  
+  // Change description text if the song is loading, playing or paused
+  function updateDescription() {
+    if (!soundFile.isPlaying()) {
+      description = 'Paused...';
+      pTag.html(description);
+    }
+    else if (soundFile.isPlaying()){
+      description = 'Playing!';
+      pTag.html(description);
+    }
+    else {
+      for (let i = 0; i < p.frameCount%3; i++ ) {
+        // add periods to loading to create a fun loading bar effect
+        // 読み込み中にピリオドを追加して、楽しい読み込みバー効果を作成します。
+        // todo: 表示されなくない？
+        if (p.frameCount%4 == 0){
+          description += '.';
+        }
+        if (p.frameCount%25 == 0) {
+          description = 'loading';
+        }
+      }
+      pTag.html(description);
+    }
+  }
 
   /*
   p.mousePressed = (e) => {
