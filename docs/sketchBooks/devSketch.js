f4640e36c74a717c2cc5b5397ccc776ec0d23e66
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-09-15 16:58:57 +0900

    up

--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -20,6 +20,9 @@ const sketch = (p) => {
 
   let pointerTracker;
   let tapIndicator;
+  
+  let description = 'loading';
+  let pTag1, pTag2;
 
   p.preload = () => {
     p.loadModule(interactionTraceKitPath, (m) => {
@@ -48,11 +51,16 @@ const sketch = (p) => {
 
     fft = new p5.FFT();
     
+    pTag1 = [w * 0.5, h * 0.3];
+    p.textAlign(p.CENTER);
+    p.text(description, ...pTag1);
     
     
     p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {
       passive: false,
     });
+    
+    
     tapIndicator.setup();
     
   };
@@ -60,6 +68,13 @@ const sketch = (p) => {
   p.draw = () => {
     // put drawing code here
     p.background(30);
+    
+    // Map mouseX to a bandpass freq from the FFT spectrum range: 10Hz - 22050Hz
+    filterFreq = p.map (p.mouseX, 0, w, 10, 22050);
+    // Map mouseY to resonance/width
+    filterWidth = p.map(p.mouseY, 0, h, 0, 90);
+    // set filter parameters
+    filter.set(filterFreq, filterWidth);
     
     
     // Draw every value in the FFT spectrum analysis where
@@ -74,15 +89,17 @@ const sketch = (p) => {
       const _h = -h + p.map(value, 0, 255, h, 0);
       p.rect(x, h, w/length, _h) ;
     });
-    /*
-    for (let i = 0; i< spectrum.length; i++){
-      const x = p.map(i, 0, spectrum.length, 0, h);
-      const _h = -h + p.map(spectrum[i], 0, 255, h, 0);
-      p.rect(x, h, w/spectrum.length, _h) ;
-    }
-    */
+    updateDescription();
+    
   };
 
+  function updateDescription() {
+    p.push();
+    p.fill(255 - 30);
+    description = 'Playing! Press any key to pause.\nFilter Frequency = ' + filterFreq + '\nFilter Width = ' + filterWidth;
+    p.text(description, ...pTag1);
+    p.pop();
+  }
   
   p.touchStarted = (e) => {
   };
@@ -102,59 +119,3 @@ const sketch = (p) => {
 
 new p5(sketch);
 
-
-
-var description = 'loading';
-var p;
-var noise;
-var fft;
-var filter, filterFreq, filterWidth;
-
-function setup() {
-  createCanvas(710, 256);
-  fill(255, 40, 255);
-
-  filter = new p5.BandPass();
-
-  noise = new p5.Noise();
-
-  noise.disconnect(); // Disconnect soundfile from main output...
-  filter.process(noise); // ...and connect to filter so we'll only hear BandPass.
-  noise.start();
-
-  fft = new p5.FFT();
-
-  // update description text
-  p = createP(description);
-  var p2 = createP('Draw the array returned by FFT.analyze( ). This represents the frequency spectrum, from lowest to highest frequencies.');
-}
-
-function draw() {
-  background(30);
-
-  // Map mouseX to a bandpass freq from the FFT spectrum range: 10Hz - 22050Hz
-  filterFreq = map (mouseX, 0, width, 10, 22050);
-  // Map mouseY to resonance/width
-  filterWidth = map(mouseY, 0, height, 0, 90);
-  // set filter parameters
-  filter.set(filterFreq, filterWidth);
-
-  // Draw every value in the FFT spectrum analysis where
-  // x = lowest (10Hz) to highest (22050Hz) frequencies,
-  // h = energy / amplitude at that frequency
-  var spectrum = fft.analyze();
-  noStroke();
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width/spectrum.length, h) ;
-  }
-
-  updateDescription();
-}
-
-// display current Filter params
-function updateDescription() {
-    description = 'Playing! Press any key to pause. Filter Frequency = ' + filterFreq + ' Filter Width = ' + filterWidth;
-    p.html(description);
-}
