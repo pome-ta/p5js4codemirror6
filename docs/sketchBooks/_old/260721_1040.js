379bd9ffa34ddb51791c0bf0a548e74176397a24
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-21 11:40:03 +0900

    PointerTracker

---
+++ docs/sketchBooks/_old/260721_1040.js
@@ -0,0 +1,47 @@
+const sketch = (p) => {
+  const v = 360;
+  let osc, playing, freq, amp;
+  let musicalScale = [261.6, 311.1, 349.2, 370, 392, 466.2];
+
+  p.setup = () => {
+    // put setup code here
+    const cnv = p.createCanvas(v, v);
+    cnv.mouseReleased(p.userStartAudio);
+    p.colorMode(p.HSL, v, 1, 1);
+
+    osc = new p5.Oscillator('sawtooth');
+
+    let s = p.second();
+    // console.log(s);
+    console.log(s % musicalScale.length);
+    // osc.freq(440);
+    osc.freq(musicalScale[s % musicalScale.length]);
+    osc.amp(0.1);
+    osc.start();
+  };
+
+  p.draw = () => {
+    // put drawing code here
+    p.background(p.frameCount % v, 1, 0.5);
+    freq = musicalScale[p.floor(p.map(p.mouseX, 0, p.width, 0, 5))];
+
+    amp = p.constrain(p.map(p.mouseY, p.height, 0, 0, 1), 0, 1);
+    p.text('tap to play', 20, 20);
+    p.text('freq: ' + freq, 20, 40);
+    p.text('amp: ' + amp, 20, 60);
+
+    if (playing == true) {
+      osc.freq(freq);
+      osc.amp(amp);
+    }
+  };
+
+  function playOscillator() {
+    // starting an oscillator on a user gesture will enable audio
+    // in browsers that have a strict autoplay policy.
+    osc.start();
+    playing = true;
+  }
+};
+
+new p5(sketch);
--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -1,47 +1,58 @@
+class PointerTracker {
+  #p;
+
+  constructor(mainInstance) {
+    this.#p = mainInstance;
+
+    this.x = null;
+    this.y = null;
+
+    [this.click, this.start, this.move, this.end, this.isTouchDevice] = window.matchMedia('(hover: none)').matches
+      ? ['click', 'touchstart', 'touchmove', 'touchend', true]
+      : ['click', 'mousedown', 'mousemove', 'mouseup', false];
+  }
+
+  updateXY() {
+    this.isTouchDevice ? this.#touchUpdate() : this.#mouseUpdate();
+  }
+
+  #touchUpdate() {
+    // xxx: 最初の指だけなら`[0]`、マルチなら座標並列
+    for (let touch of this.#p.touches) {
+      this.x = 0 <= touch.x && touch.x <= this.#p.width ? touch.x : null;
+      this.y = 0 <= touch.y && touch.y <= this.#p.height ? touch.y : null;
+    }
+  }
+
+  #mouseUpdate() {
+    if (!this.#p.mouseIsPressed) {
+      this.x = null;
+      this.y = null;
+      return;
+    }
+    this.x = 0 <= this.#p.mouseX && this.#p.mouseX <= this.#p.width ? this.#p.mouseX : null;
+    this.y = 0 <= this.#p.mouseY && this.#p.mouseY <= this.#p.height ? this.#p.mouseY : null;
+  }
+}
+
 const sketch = (p) => {
   const v = 360;
-  let osc, playing, freq, amp;
-  let musicalScale = [261.6, 311.1, 349.2, 370, 392, 466.2];
+  let pt;
 
   p.setup = () => {
     // put setup code here
-    const cnv = p.createCanvas(v, v);
-    cnv.mouseReleased(p.userStartAudio);
+    p.createCanvas(v, v);
     p.colorMode(p.HSL, v, 1, 1);
-
-    osc = new p5.Oscillator('sawtooth');
-
-    let s = p.second();
-    // console.log(s);
-    console.log(s % musicalScale.length);
-    // osc.freq(440);
-    osc.freq(musicalScale[s % musicalScale.length]);
-    osc.amp(0.1);
-    osc.start();
+    pt = new PointerTracker(p);
   };
 
   p.draw = () => {
     // put drawing code here
     p.background(p.frameCount % v, 1, 0.5);
-    freq = musicalScale[p.floor(p.map(p.mouseX, 0, p.width, 0, 5))];
-
-    amp = p.constrain(p.map(p.mouseY, p.height, 0, 0, 1), 0, 1);
-    p.text('tap to play', 20, 20);
-    p.text('freq: ' + freq, 20, 40);
-    p.text('amp: ' + amp, 20, 60);
-
-    if (playing == true) {
-      osc.freq(freq);
-      osc.amp(amp);
-    }
+    pt.updateXY();
+    p.text(`x: ${pt.x}`, 20, 140);
+    p.text(`y: ${pt.y}`, 20, 160);
   };
-
-  function playOscillator() {
-    // starting an oscillator on a user gesture will enable audio
-    // in browsers that have a strict autoplay policy.
-    osc.start();
-    playing = true;
-  }
 };
 
 new p5(sketch);
