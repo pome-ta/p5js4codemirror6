4072a9adad09048159c2999c0ded567657ef2e61
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-22 02:38:58 +0900

    up

--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -35,23 +35,154 @@ class PointerTracker {
   }
 }
 
+class TapIndicator {
+  #p;
+  #pg;
+  #pointerTracker;
+  #markSize;
+  #pgColor;
+
+  baseColorHSB = [0.3, 0.1, 0.9];
+
+  constructor(mainInstance, markSize = 48) {
+    this.#p = mainInstance;
+    this.#pg = null;
+    this.#pointerTracker = new PointerTracker(mainInstance);
+    this.#markSize = markSize;
+
+    this.isTapped = null;
+  }
+
+  setup() {
+    this.isTapped = false;
+
+    this.#initCreateGraphics();
+    this.#setUseHooks();
+  }
+
+  #initCreateGraphics = () => {
+    this.#pg && this.#pg.remove();
+    this.#pg = this.#p.createGraphics(this.#p.width, this.#p.height);
+
+    this.#pg.colorMode(this.#pg.HSB, 1.0, 1.0, 1.0, 1.0);
+    this.#pg.blendMode(this.#pg.SCREEN);
+    this.#pgColor = this.#pg.color(...this.baseColorHSB);
+    this.#pgColor.setAlpha(0.5);
+    this.#pg.fill(this.#pgColor);
+    this.#pg.noStroke();
+
+    this.#pg.ellipseMode(this.#pg.CENTER);
+  };
+
+  #showMark = () => {
+    this.#pg.circle(this.#pointerTracker.x, this.#pointerTracker.y, this.#markSize);
+    this.#p.image(this.#pg, 0, 0);
+  };
+
+  #drawHook = () => {
+    this.#pg.clear();
+    if (!this.isTapped || this.#pointerTracker.x === null || this.#pointerTracker.y === null) {
+      return;
+    }
+    this.#showMark();
+  };
+
+  #touchStartedHook = (e) => {
+    this.isTapped = true;
+    this.#pointerTracker.updateXY();
+  };
+  #touchMovedHook = (e) => {
+    this.#pointerTracker.updateXY();
+  };
+  #touchEndedHook = (e) => {
+    this.isTapped = false;
+    // xxx: `ended` 判定で`null` が取れるが必要か?
+    this.#pointerTracker.updateXY();
+  };
+
+  #setUseHooks = () => {
+    this.#useDraw();
+    this.#useTouchEvents();
+    this.#useWindowResized();
+  };
+
+  #useDraw() {
+    const instance = this;
+    const originalFunction = instance.#p.draw;
+
+    instance.#p.draw = function (...args) {
+      const result = originalFunction.apply(this, args);
+      instance.#drawHook();
+      return result;
+    };
+  }
+
+  #useTouchEvents() {
+    const instance = this;
+
+    // touchStarted
+    const touchStartedFunction = instance.#p.touchStarted === void 0 ? (e) => {} : instance.#p.touchStarted;
+    instance.#p.touchStarted = function (...args) {
+      const result = touchStartedFunction.apply(this, args);
+      instance.#touchStartedHook(args);
+      return result;
+    };
+
+    // touchMoved
+    const touchMovedFunction = instance.#p.touchMoved === void 0 ? (e) => {} : instance.#p.touchMoved;
+    instance.#p.touchMoved = function (...args) {
+      const result = touchMovedFunction.apply(this, args);
+      instance.#touchMovedHook(args);
+      return result;
+    };
+
+    // touchEnded
+    const touchEndedFunction = instance.#p.touchEnded === void 0 ? (e) => {} : instance.#p.touchEnded;
+    instance.#p.touchEnded = function (...args) {
+      const result = touchEndedFunction.apply(this, args);
+      instance.#touchEndedHook(args);
+      return result;
+    };
+  }
+
+  #useWindowResized() {
+    const instance = this;
+    const originalFunction = instance.#p.windowResized === void 0 ? (e) => {} : instance.#p.windowResized;
+    instance.#p.windowResized = function (...args) {
+      const result = originalFunction.apply(this, args);
+      instance.#initCreateGraphics();
+      return result;
+    };
+  }
+}
+
 const sketch = (p) => {
   const v = 360;
-  let pt;
+  let tapIndicator;
+  let pointerTracker;
 
   p.setup = () => {
+    //p.noCanvas();
     // put setup code here
     p.createCanvas(v, v);
     p.colorMode(p.HSL, v, 1, 1);
-    pt = new PointerTracker(p);
+    
+    tapIndicator = new TapIndicator(p);  
+    pointerTracker = new PointerTracker(p);  
+      
+    // キャンバス作成後にsetupを呼ぶ  
+    tapIndicator.setup();  
+      
+    p.canvas.addEventListener(pointerTracker.move, (e) => e.preventDefault(), {  
+      passive: false,  
+    });  
+
   };
 
   p.draw = () => {
     // put drawing code here
     p.background(p.frameCount % v, 1, 0.5);
-    pt.updateXY();
-    p.text(`x: ${pt.x}`, 20, 140);
-    p.text(`y: ${pt.y}`, 20, 160);
+
   };
 };
 
