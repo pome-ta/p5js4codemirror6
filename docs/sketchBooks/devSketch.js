f03a050dfbc4e4ced2588ca397a2d9a516ea8177
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-22 21:36:32 +0900

    up

--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -1,24 +1,20 @@
 import TapIndicator from '../../sketchBooks/modules/TapIndicator.js';
 
-
 const sketch = (p) => {
   const v = 360;
   let tapIndicator;
-  
+
   p.setup = () => {
-    //p.noCanvas();
     // put setup code here
     p.createCanvas(v, v);
     p.colorMode(p.HSL, v, 1, 1);
 
     tapIndicator = new TapIndicator(p);
-    //p.frameRate(8);
   };
 
   p.draw = () => {
     // put drawing code here
     p.background(p.frameCount % v, 1, 0.5);
-    //tapIndicator.destroy()
 
     p.strokeWeight(30);
     p.stroke('blue');
