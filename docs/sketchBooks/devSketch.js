a8d505218996742dad3c11c45f14b6c733a6f5eb
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-07-23 14:42:22 +0900

    `modules` path

--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -1,32 +1,28 @@
-
-import TapIndicator from '../../sketchBooks/modules/TapIndicator.js';
+import TapIndicator from 'modules/TapIndicator.js';
 
 const sketch = (p) => {
-  let tapIndicator = new TapIndicator(p);
+  let tapIndicator;
 
   let sound;
   let amp;
-  
 
   //p.setup = async () => {
   p.setup = () => {
     // put setup code here
-    //tapIndicator = new TapIndicator(p);
+    tapIndicator = new TapIndicator(p);
 
     p.createCanvas(400, 400);
     p.textAlign(p.CENTER);
     p.fill(25);
 
-
-
-    p.describe('The color of the background changes based on the amplitude of the sound.');
+    p.describe(
+      'The color of the background changes based on the amplitude of the sound.',
+    );
   };
 
   p.draw = () => {
     // put drawing code here
   };
-
 };
 
 new p5(sketch);
-
