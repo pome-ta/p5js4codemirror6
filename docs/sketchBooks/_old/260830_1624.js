c99a17da7e23ece66231875fcc6b493e14f3740e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-08-30 16:24:47 +0900

    up

---
+++ docs/sketchBooks/_old/260830_1624.js
@@ -0,0 +1,237 @@
+// --- # example: pulse LFO & lowpass
+
+import * as Tone from 'tone';
+
+import TapIndicator from 'modules/TapIndicator.js';
+import SpectrumAnalyzer from 'modules/SpectrumAnalyzer.js';
+
+const sketch = (p) => {
+  // --- Plugins
+  const tapIndicator = new TapIndicator(p);
+  const spectrumAnalyzer = new SpectrumAnalyzer(p, 2048);
+
+  // --- Tone.js
+  const ctx = p.getAudioContext();
+  Tone.setContext(ctx);
+  const transport = Tone.getTransport();
+  const BPM = transport.bpm;
+
+  let bpm = 100;
+
+  let masterCh;
+  let synth;
+  let tFilter;
+
+  // --- Sketch
+  let cnvs;
+  let w = p.windowWidth;
+  let h = p.windowHeight;
+
+  let pointerId = null;
+  let xyPad;
+
+  const holdColor = 'rgba(128, 0, 0, 0.64)';
+  const holdAlpha = 0.4;
+  const idleAlpha = 0.12;
+  const idleBg = (a) => `rgba(0, 0, 128, ${a})`;
+
+  const notes = ['D2', 'F2', 'A2', 'A3', 'D3', 'F2', 'A2'];
+  let callCounter = 0;
+
+  p.setup = () => {
+    // put setup code here
+    cnvs = p.createCanvas(w, h);
+
+    BPM.value = bpm;
+
+    synth = new Tone.MonoSynth({
+      oscillator: { type: 'pulse', width: 0 },
+      envelope: { attack: 0.005, decay: 0.9, sustain: 0.8, release: 1 },
+    });
+
+    //synth = new Tone.Synth({ oscillator: { type: 'pulse', width: 0 } });
+
+    const lfo = new Tone.LFO('1n', -0.8, 0.8).start();
+    //lfo.connect(synth.oscillator.width);
+
+    //tFilter = new Tone.Filter(1500, 'highpass');
+    tFilter = new Tone.Filter(880, 'lowpass');
+
+    // --- mixer
+    masterCh = new Tone.Channel().toDestination();
+    synth.chain(tFilter, masterCh);
+
+    tapIndicator.setup();
+    spectrumAnalyzer.targetNodes(masterCh);
+    domSetup();
+
+    //p.noLoop();
+  };
+
+  /* tone 操作 */
+  const moveWidth = (v) => {
+    const valueWidth = p.map(v, 0, 1, -0.95, 0.95);
+    synth.oscillator.width.value = valueWidth;
+  };
+
+  const moveFilter = (v) => {
+    const value = p.map(v, 0, 1, 20, 12000);
+    tFilter.frequency.value = value;
+  };
+
+  const toneOperation = {
+    pointerdown: (ratioPointer) => {
+      synth.triggerAttack(notes[callCounter++ % notes.length]);
+      moveWidth(ratioPointer.y);
+    },
+    pointermove: (ratioPointer) => {
+      moveWidth(ratioPointer.y);
+      moveFilter(ratioPointer.x);
+    },
+    pointerup: () => {
+      synth.triggerRelease();
+    },
+    pointercancel: () => {
+      synth?.triggerRelease();
+    },
+  };
+
+  p.draw = () => {
+    // put drawing code here
+    p.background(80);
+    //p.rect(0, 0, w / 2, h / 2);
+    spectrumAnalyzer.drawGraph();
+  };
+
+  p.windowResized = (e) => {
+    console.log('windowResized');
+    w = p.windowWidth;
+    h = p.windowHeight;
+    cnvs = p.resizeCanvas(w, h);
+    domLayout();
+  };
+
+  const xyPadClientFrame = (event) => {
+    const {
+      left: rectLeft,
+      top: rectTop,
+      width: rectWidth,
+      height: rectHeight,
+    } = event.currentTarget.getBoundingClientRect();
+
+    // xxx: 外の要素まで拾わなくていいと思うのだけど・・・
+    const absPointer = {
+      x: p.map(event.clientX - rectLeft, 0, rectWidth, 0, rectWidth, true),
+      y: p.map(event.clientY - rectTop, 0, rectHeight, 0, rectHeight, true),
+    };
+    const ratioPointer = {
+      x: p.map(absPointer.x, 0, rectWidth, 0.0, 1.0, true),
+      y: p.map(absPointer.y, 0, rectHeight, 0.0, 1.0, true),
+    };
+
+    return {
+      absPointer,
+      ratioPointer,
+      size: { width: rectWidth, height: rectHeight },
+      position: { x: rectLeft, y: rectTop },
+      client: { x: event.clientX, y: event.clientY },
+    };
+  };
+
+  const domSetup = () => {
+    /* dom (xyPad) 定義 */
+    xyPad = p.createDiv();
+    xyPad
+      .style('width', '16rem')
+      .style('height', '16rem')
+      .style('background', idleBg(idleAlpha))
+      .style('-webkit-touch-callout', 'none')
+      .style('-webkit-user-select', 'none')
+      .style('user-select', 'none')
+      .style('touch-action', 'none');
+
+    /* xyPad Action */
+    const styleTransformPerspective = (ratioPointer) => {
+      const xMap = p.map(ratioPointer.y, 0, 1, -7.5, 7.5, true);
+      const yMap = p.map(ratioPointer.x, 0, 1, 7.5, -7.5, true);
+
+      return `rotateY(${yMap}deg) rotateX(${xMap}deg)`;
+    };
+
+    const styleRadialGradient = (absPointer) => {
+      const stylePos = `circle at ${absPointer.x}px ${absPointer.y}px `;
+      const selectColors = `${holdColor} 8%, ${idleBg(holdAlpha)}  1%`;
+
+      return `radial-gradient(${stylePos} in hsl longer hue, ${selectColors})`;
+    };
+
+    const xyPadAction = (ratioPointer, absPointer) => {
+      xyPad.style(
+        'transform',
+        `perspective(16rem) ${styleTransformPerspective(ratioPointer)}`,
+      );
+      xyPad.style('background', `${styleRadialGradient(absPointer)}`);
+    };
+
+    const idleSignal = (event) => {
+      xyPad.elt.releasePointerCapture(event.pointerId);
+      xyPad.style('background', idleBg(idleAlpha));
+      pointerId = null;
+    };
+
+    /* pointer event 定義 */
+    const eventlLiteral = {
+      pointerdown: (event) => {
+        xyPad.elt.setPointerCapture(event.pointerId);
+        pointerId = event.pointerId;
+        const { ratioPointer: rp, absPointer: ap } = xyPadClientFrame(event);
+
+        xyPadAction(rp, ap);
+        toneOperation.pointerdown(rp);
+      },
+
+      pointermove: (event) => {
+        if (event.buttons === 0 || event.pointerId !== pointerId) {
+          pointerId = null;
+          return;
+        }
+        const { ratioPointer: rp, absPointer: ap } = xyPadClientFrame(event);
+
+        xyPadAction(rp, ap);
+        toneOperation.pointermove(rp);
+      },
+
+      pointerup: (event) => {
+        idleSignal(event);
+        toneOperation.pointerup();
+      },
+
+      pointercancel: (event) => {
+        console.log('pointercancel');
+        idleSignal(event);
+        toneOperation.pointercancel();
+      },
+    };
+
+    const signalEvent = (event) => {
+      eventlLiteral[event.type](event);
+    };
+    xyPad.mousePressed(signalEvent);
+    xyPad.mouseMoved(signalEvent);
+    xyPad.mouseReleased(signalEvent);
+
+    domLayout();
+  };
+
+  const domLayout = () => {
+    // console.log('layout');
+    const cw = xyPad.size().width;
+    const ch = xyPad.size().height;
+    const x = w / 2 - cw / 2;
+    const y = h / 2 - ch / 2;
+
+    xyPad.position(x, y / 2);
+  };
+};
+
+new p5(sketch);
