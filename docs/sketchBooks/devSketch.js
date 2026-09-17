acb4648ebf75a3b36a4f008b5332a30dad372a48
pome-ta <53405097+pome-ta@users.noreply.github.com>
2026-09-17 22:43:47 +0900

    up

--- docs/sketchBooks/devSketch.js
+++ docs/sketchBooks/devSketch.js
@@ -6,6 +6,15 @@ import TapIndicator from 'modules/TapIndicator.js';
 import SpectrumAnalyzer from 'modules/SpectrumAnalyzer.js';
 
 const sketch = (p) => {
+  // --- Sketch
+  let cnvs;
+  let w = p.windowWidth;
+  let h = p.windowHeight;
+
+  // --- Plugins
+  const tapIndicator = new TapIndicator(p);
+  const spectrumAnalyzer = new SpectrumAnalyzer(p, 2048);
+
   // --- Tone.js
   const ctx = p.getAudioContext();
   Tone.setContext(ctx);
@@ -15,21 +24,20 @@ const sketch = (p) => {
   });
 
   const transport = Tone.getTransport();
-  transport.bpm.value = 105;
+  transport.bpm.value = 120;
 
   const masterCh = new Tone.Channel().toDestination();
 
-  const drumCh = new Tone.Channel();
   const kick = 'kick',
     snare = 'snare';
-  const drumKitPlayer = new Tone.Players();
-  drumKitPlayer.fadeIn = '1i';
-  drumKitPlayer.fadeOut = '1i';
+  const drumKit = new Tone.Players();
+  drumKit.fadeIn = '1i';
+  drumKit.fadeOut = '1i';
 
   const drumSeq = new Tone.Sequence({
     callback: (time, nameTrigger) => {
       Object.values(nameTrigger).forEach((trigger) => {
-          trigger.start(time);
+        drumKit.player(trigger).start(time);
       });
     },
     // prettier-ignore
@@ -40,16 +48,14 @@ const sketch = (p) => {
     // humanize: 0.001,
   });
 
+  const drumCh = new Tone.Channel();
+  const drumChainAry = [
+    //
+    drumCh,
+  ];
+  drumKit.chain(...drumChainAry.filter((n) => n));
+
   drumCh.chain(masterCh);
-
-  // --- Sketch
-  let cnvs;
-  let w = p.windowWidth;
-  let h = p.windowHeight;
-
-  // --- Plugins
-  const tapIndicator = new TapIndicator(p);
-  const spectrumAnalyzer = new SpectrumAnalyzer(p, 2048);
 
   p.setup = async () => {
     // put setup code here
@@ -57,36 +63,35 @@ const sketch = (p) => {
 
     const kickBuffer = await Tone.Offline(() => {
       const synth = new Tone.Synth({
-        // oscillator: { type: 'sine', phase: -80 },
+        oscillator: { type: 'sine', phase: -80 },
         // oscillator: { type: 'sine' },
         // oscillator: { type: 'sine', phase: -72 },
 
         oscillator: { type: 'pulse', width: 0 },
         envelope: {
-          attack: 1e-3,
+          // attack: 1e-3,
+          attack: 0,
+
           decay: 12.5,
           sustain: 0.0,
-          release: 0.075,
+          release: '1i',
           attackCurve: 'exponential',
           // releaseCurve: 'exponential',
         },
         //portamento: 0.125,
       }).toDestination();
-      const nowTime = Tone.now();
-      // console.log(nowTime);
 
-      synth.triggerAttack('A6', 0);
+      synth.triggerAttack('A3', 0);
       // synth.frequency.rampTo('A2', 0.1);
       // synth.triggerAttackRelease('A5', nowTime, nowTime+1.5);
-      synth.frequency.rampTo('A3', 0.1);
+      synth.frequency.rampTo('A0', `32i`);
       // synth.triggerAttack('A0', nowTime + 0.001);
       // synth.triggerAttackRelease('A4', 0, 1.5);
 
-      synth.triggerRelease(0.5);
-    }, 2.5);
-    
-    drumKitPlayer.add(kick,kickBuffer);
-    
+      synth.triggerRelease('128i');
+    }, 2);
+
+    drumKit.add(kick, kickBuffer);
 
     transport.start(0);
     drumSeq.start();
